1feb6b1a6f1cfbf2134744afe81ef41e6c2db7ce
Tom Wolfe <trwolfe13@gmail.com>
2021-10-15 15:53:06 +0100

    feature: Solve day 2

--- src/Wolfe.AdventOfCode.2020/Puzzles/Day01.cs
+++ src/Wolfe.AdventOfCode.2020/Puzzles/Day01.cs
@@ -5,28 +5,27 @@ namespace Wolfe.AdventOfCode._2020.Puzzles
 {
     internal class Day01 : IPuzzleDay
     {
-        private readonly SumFinder _sumFinder;
+        public int Day { get; } = 1;
 
-        public Day01()
+        private static readonly Lazy<SumFinder> LazySumFinder = new(() =>
         {
             var input = File
                 .ReadAllLines("./Inputs/Day01.txt")
                 .Select(int.Parse);
-            _sumFinder = new SumFinder(input);
-        }
-
-        public int Day { get; } = 1;
+            return new SumFinder(input);
+        });
+        private static SumFinder SumFinder => LazySumFinder.Value;
 
         public Task<string> Part1(CancellationToken cancellationToken = default)
         {
-            var (a, b) = _sumFinder.FindPair(2020);
+            var (a, b) = SumFinder.FindPair(2020);
             var solution = a * b;
             return Task.FromResult(solution.ToString());
         }
 
         public Task<string> Part2(CancellationToken cancellationToken = default)
         {
-            var (a, b, c) = _sumFinder.FindTriplet(2020);
+            var (a, b, c) = SumFinder.FindTriplet(2020);
             var solution = a * b * c;
             return Task.FromResult(solution.ToString());
         }
--- src/Wolfe.AdventOfCode.2020/Puzzles/Day02.cs
+++ src/Wolfe.AdventOfCode.2020/Puzzles/Day02.cs
@@ -5,21 +5,29 @@ namespace Wolfe.AdventOfCode._2020.Puzzles
 {
     internal class Day02 : IPuzzleDay
     {
-        private static readonly Regex PasswordRegex = new (@"(?<min>\d+)-(?<max>\d+) (?<letter>\D): (?<password>\D+)");
         public int Day { get; } = 2;
+
+        private static readonly Regex PasswordRegex = new (@"(?<min>\d+)-(?<max>\d+) (?<letter>\D): (?<password>\D+)");
+        private static readonly Lazy<List<PasswordModel>> LazyPasswords = new(() => File
+            .ReadAllLines("./Inputs/Day02.txt")
+            .Select(ParsePassword)
+            .ToList()
+        );
+        private static List<PasswordModel> Passwords => LazyPasswords.Value;
 
         public Task<string> Part1(CancellationToken cancellationToken = default)
         {
-            var input = File
-                .ReadAllLines("./Inputs/Day02.txt")
-                .Select(ParsePassword);
-            var validPasswords = input.Count(IsValid);
+            var validPasswords = Passwords.Count(IsValid);
             return Task.FromResult(validPasswords.ToString());
         }
 
         public Task<string> Part2(CancellationToken cancellationToken = default)
         {
-            return Task.FromResult("0");
+            var input = File
+                .ReadAllLines("./Inputs/Day02.txt")
+                .Select(ParsePassword);
+            var validPasswords = input.Count(IsValidPart2);
+            return Task.FromResult(validPasswords.ToString());
         }
 
         record PasswordModel(int Min, int Max, char Letter, string Password);
@@ -35,10 +43,15 @@ namespace Wolfe.AdventOfCode._2020.Puzzles
             );
         }
 
-        private static bool IsValid(PasswordModel password)
+        private static bool IsValid(PasswordModel pwd)
         {
-            var count = password.Password.Count(c => c == password.Letter);
-            return count >= password.Min && count <= password.Max;
+            var count = pwd.Password.Count(c => c == pwd.Letter);
+            return count >= pwd.Min && count <= pwd.Max;
+        }
+
+        private static bool IsValidPart2(PasswordModel pwd)
+        {
+            return pwd.Password[pwd.Min - 1] == pwd.Letter ^ pwd.Password[pwd.Max - 1] == pwd.Letter;
         }
     }
 }
