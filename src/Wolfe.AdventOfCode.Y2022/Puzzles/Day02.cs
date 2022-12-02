b0fc2582230eca454e295a1d216469ce4f8195e2
Tom Wolfe <trwolfe13@gmail.com>
2022-12-02 14:10:05 +0000

    feat: optimise

--- src/Wolfe.AdventOfCode.Y2022/Puzzles/Day02.cs
+++ src/Wolfe.AdventOfCode.Y2022/Puzzles/Day02.cs
@@ -1,4 +1,50 @@
-﻿namespace Wolfe.AdventOfCode.Y2022.Puzzles;
+﻿using System.Text;
+
+namespace Wolfe.AdventOfCode.Y2022.Puzzles;
+
+enum RockPaperScissors
+{
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3
+}
+
+record Round(RockPaperScissors You, RockPaperScissors Opponent)
+{
+    public int Score => (int)You + (Opponent == You ? 3 : You == LosesAgainst(Opponent) ? 6 : 0);
+
+    public static Round FromInstruction(string input)
+    {
+        return new Round(Parse(input[2]), Parse(input[0]));
+    }
+
+    public static Round FromWinState(string input)
+    {
+        var opponent = Parse(input[0]);
+        var you = input[2] switch
+        {
+            'X' => WinsAgainst(opponent),
+            'Y' => opponent,
+            'Z' => LosesAgainst(opponent),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+        return new Round(you, opponent);
+    }
+
+    private static RockPaperScissors LosesAgainst(RockPaperScissors input) => (RockPaperScissors)((int)input % 3 + 1);
+    private static RockPaperScissors WinsAgainst(RockPaperScissors input) => (RockPaperScissors)(((int)input + 1) % 3 + 1);
+
+    private static RockPaperScissors Parse(char c) => c switch
+    {
+        'A' => RockPaperScissors.Rock,
+        'B' => RockPaperScissors.Paper,
+        'C' => RockPaperScissors.Scissors,
+        'X' => RockPaperScissors.Rock,
+        'Y' => RockPaperScissors.Paper,
+        'Z' => RockPaperScissors.Scissors,
+        _ => throw new ArgumentOutOfRangeException(nameof(c), c, null)
+    };
+}
 
 internal class Day02 : IPuzzleDay
 {
@@ -19,65 +65,4 @@ internal class Day02 : IPuzzleDay
         .Sum()
         .ToString()
         .ToTask();
-
-    private record Round(RockPaperScissors You, RockPaperScissors Opponent)
-    {
-        public bool IsDraw => Opponent == You;
-
-        public bool IsWin => You == LosesAgainst(Opponent);
-
-        public int Score => (int)You + (IsDraw ? 3 : 0) + (IsWin ? 6 : 0);
-
-        public static Round FromInstruction(string input)
-        {
-            return new Round(Parse(input[2]), Parse(input[0]));
-        }
-
-        public static Round FromWinState(string input)
-        {
-            var opponent = Parse(input[0]);
-            var you = input[2] switch
-            {
-                'X' => WinsAgainst(opponent),
-                'Y' => opponent,
-                'Z' => LosesAgainst(opponent),
-                _ => throw new ArgumentOutOfRangeException()
-            };
-            return new Round(you, opponent);
-        }
-
-        private static RockPaperScissors LosesAgainst(RockPaperScissors input) => input switch
-        {
-            RockPaperScissors.Rock => RockPaperScissors.Paper,
-            RockPaperScissors.Paper => RockPaperScissors.Scissors,
-            RockPaperScissors.Scissors => RockPaperScissors.Rock,
-            _ => throw new ArgumentOutOfRangeException(nameof(input), input, null)
-        };
-
-        private static RockPaperScissors WinsAgainst(RockPaperScissors input) => input switch
-        {
-            RockPaperScissors.Rock => RockPaperScissors.Scissors,
-            RockPaperScissors.Paper => RockPaperScissors.Rock,
-            RockPaperScissors.Scissors => RockPaperScissors.Paper,
-            _ => throw new ArgumentOutOfRangeException(nameof(input), input, null)
-        };
-
-        private static RockPaperScissors Parse(char c) => c switch
-        {
-            'A' => RockPaperScissors.Rock,
-            'B' => RockPaperScissors.Paper,
-            'C' => RockPaperScissors.Scissors,
-            'X' => RockPaperScissors.Rock,
-            'Y' => RockPaperScissors.Paper,
-            'Z' => RockPaperScissors.Scissors,
-            _ => throw new ArgumentOutOfRangeException(nameof(c), c, null)
-        };
-    }
-
-    enum RockPaperScissors
-    {
-        Rock = 1,
-        Paper = 2,
-        Scissors = 3
-    }
 }
