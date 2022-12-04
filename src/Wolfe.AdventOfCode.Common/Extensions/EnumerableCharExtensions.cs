d23a172aba9d5308e513f6be429dc4760bf1a9c7
Tom Wolfe <trwolfe13@gmail.com>
2022-12-04 13:33:56 +0000

    feat: day 4 part 1

--- src/Wolfe.AdventOfCode.Common/Extensions/EnumerableCharExtensions.cs
+++ src/Wolfe.AdventOfCode.Common/Extensions/EnumerableCharExtensions.cs
@@ -1,6 +1,20 @@
 ﻿namespace Wolfe.AdventOfCode.Extensions;
 
-public static class EnumerableCharExtensions
+public static class EnumerableExtensions
 {
-    public static string Join(this IEnumerable<char> chars) => new(chars.ToArray());
+    public static void Deconstruct<T>(this IEnumerable<T> source, out T v1)
+    {
+        using var e = source.GetEnumerator();
+        e.MoveNext();
+        v1 = e.Current;
+    }
+
+    public static void Deconstruct<T>(this IEnumerable<T> source, out T v1, out T v2)
+    {
+        using var e = source.GetEnumerator();
+        e.MoveNext();
+        v1 = e.Current;
+        e.MoveNext();
+        v2 = e.Current;
+    }
 }
---
+++ src/Wolfe.AdventOfCode.Y2022/Puzzles/Day04.cs
@@ -0,0 +1,33 @@
+﻿namespace Wolfe.AdventOfCode.Y2022.Puzzles;
+
+internal class Day04 : IPuzzleDay
+{
+    public int Day => 4;
+
+    public Task<string> Part1(string? input, CancellationToken cancellationToken = default) => input
+        .ToLines()
+        .Select(Parse)
+        .Where(FullyContains)
+        .Count()
+        .ToString()
+        .ToTask();
+
+    public Task<string> Part2(string? input, CancellationToken cancellationToken = default) => ""
+        .ToTask();
+
+    private static IEnumerable<int[]> Parse(string input) => input
+        .Split(',')
+        .Select(ParseRange);
+
+    private static int[] ParseRange(string range)
+    {
+        var (from, to) = range.Split('-').Select(int.Parse);
+        return Enumerable.Range(from, to - from + 1).ToArray();
+    }
+
+    private static bool FullyContains(IEnumerable<int[]> input)
+    {
+        var (l, r) = input;
+        return l.Intersect(r).Count() == Math.Min(l.Length, r.Length);
+    }
+}
