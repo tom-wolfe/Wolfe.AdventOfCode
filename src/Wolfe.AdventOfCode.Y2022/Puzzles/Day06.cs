a4c75a6efb2a10d407207efafca58fab52d3143c
Tom Wolfe <trwolfe13@gmail.com>
2022-12-06 11:19:18 +0000

    feat: day 6 part 1

---
+++ src/Wolfe.AdventOfCode.Y2022/Puzzles/Day06.cs
@@ -0,0 +1,36 @@
+﻿namespace Wolfe.AdventOfCode.Y2022.Puzzles;
+
+internal partial class Day06 : IPuzzleDay
+{
+    public int Day => 6;
+
+    public Task<string> Part1(string? input, CancellationToken cancellationToken = default)
+    {
+        const int sequenceLength = 4;
+
+        var buffer = new Queue<int>();
+        for (var x = 0; x < input!.Length; x++)
+        {
+            buffer.Enqueue(input[x]);
+            if (buffer.Count > sequenceLength)
+            {
+                buffer.Dequeue();
+            }
+
+            if (buffer.Count == sequenceLength && buffer.Distinct().Count() == sequenceLength)
+            {
+                return (x + 1).ToString().ToTask();
+            }
+        }
+
+        return "0".ToTask();
+    }
+
+    public Task<string> Part2(string? input, CancellationToken cancellationToken = default)
+    {
+
+
+        return ""
+            .ToTask();
+    }
+}
