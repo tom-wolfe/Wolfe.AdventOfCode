ac06aed7487675937c369ee76252765fec55a877
Tom Wolfe <trwolfe13@gmail.com>
2022-12-06 11:25:43 +0000

    feat: day 6 part 2

--- src/Wolfe.AdventOfCode.Y2022/Puzzles/Day06.cs
+++ src/Wolfe.AdventOfCode.Y2022/Puzzles/Day06.cs
@@ -1,36 +1,37 @@
-﻿namespace Wolfe.AdventOfCode.Y2022.Puzzles;
+﻿using Microsoft.VisualBasic.CompilerServices;
+
+namespace Wolfe.AdventOfCode.Y2022.Puzzles;
 
 internal partial class Day06 : IPuzzleDay
 {
     public int Day => 6;
 
-    public Task<string> Part1(string input, CancellationToken cancellationToken = default)
-    {
-        const int sequenceLength = 4;
+    public Task<string> Part1(string input, CancellationToken cancellationToken = default) => 
+        FindMarker(input, 4)
+            .ToString()
+            .ToTask();
+    
+    public Task<string> Part2(string input, CancellationToken cancellationToken = default) =>
+        FindMarker(input, 14)
+            .ToString()
+            .ToTask();
 
+    private static int FindMarker(string input, int length)
+    {
         var buffer = new Queue<int>();
-        for (var x = 0; x < input!.Length; x++)
+        for (var x = 0; x < input.Length; x++)
         {
             buffer.Enqueue(input[x]);
-            if (buffer.Count > sequenceLength)
+            if (buffer.Count > length)
             {
                 buffer.Dequeue();
             }
 
-            if (buffer.Count == sequenceLength && buffer.Distinct().Count() == sequenceLength)
+            if (buffer.Count == length && buffer.Distinct().Count() == length)
             {
-                return (x + 1).ToString().ToTask();
+                return (x + 1);
             }
         }
-
-        return "0".ToTask();
-    }
-
-    public Task<string> Part2(string input, CancellationToken cancellationToken = default)
-    {
-
-
-        return ""
-            .ToTask();
+        return -1;
     }
 }
