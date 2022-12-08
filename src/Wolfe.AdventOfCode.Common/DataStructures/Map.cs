f929e7e73493f4b125150a86138f38afd6ce4c87
Tom Wolfe <trwolfe13@gmail.com>
2022-12-08 23:47:51 +0000

    feat: day 8 part 1

---
+++ src/Wolfe.AdventOfCode.Common/DataStructures/Map.cs
@@ -0,0 +1,61 @@
+﻿namespace Wolfe.AdventOfCode.DataStructures
+{
+    public class Map<T>
+    {
+        private readonly T[,] _data;
+
+        public Map(IEnumerable<IEnumerable<T>> data)
+        {
+            var map = data.Select(i => i.ToArray()).ToArray();
+            _data = new T[map.Length, map[0].Length];
+
+            for (var column = 0; column < Columns; column++)
+            {
+                for (var row = 0; row < Rows; row++)
+                {
+                    this[column, row] = map[row][column];
+                }
+            }
+        }
+
+        public int Rows => _data.GetLength(1);
+        public int Columns => _data.GetLength(0);
+
+        public T this[int column, int row]
+        {
+            get => _data[column, row];
+            set => _data[column, row] = value;
+        }
+
+        public T[] GetRow(int row)
+        {
+            var rowData = new T[Columns];
+            for (var c = 0; c < Columns; c++)
+            {
+                rowData[c] = _data[c, row];
+            }
+            return rowData;
+        }
+
+        public T[] GetColumn(int column)
+        {
+            var columnData = new T[Columns];
+            for (var r = 0; r < Rows; r++)
+            {
+                columnData[r] = _data[column, r];
+            }
+            return columnData;
+        }
+
+        public IEnumerable<(int, int, T)> Flatten()
+        {
+            for (var column = 0; column < Columns; column++)
+            {
+                for (var row = 0; row < Rows; row++)
+                {
+                    yield return (column, row, this[column, row]);
+                }
+            }
+        }
+    }
+}
---
+++ src/Wolfe.AdventOfCode.Y2022/Puzzles/Day08.cs
@@ -0,0 +1,47 @@
+﻿using System.Data;
+using System.Xml.Schema;
+using Wolfe.AdventOfCode.DataStructures;
+
+namespace Wolfe.AdventOfCode.Y2022.Puzzles;
+
+internal class Day08 : IPuzzleDay
+{
+    public int Day => 8;
+
+    public Task<string> Part1(string input, CancellationToken cancellationToken = default)
+    {
+        var map = Parse(input);
+        return map
+            .Flatten()
+            .Count(n => IsVisible(map, n.Item1, n.Item2))
+            .ToString()
+            .ToTask();
+    }
+
+    public Task<string> Part2(string input, CancellationToken cancellationToken = default)
+    {
+        return "".ToTask();
+    }
+
+    private static Map<int> Parse(string input) => new(input.ToLines().Select(r => r.Select(c => int.Parse(c.ToString()))));
+
+    private static bool IsVisible(Map<int> map, int x, int y)
+    {
+        var t = map[x, y];
+
+        var h = map.GetRow(y);
+        var v = map.GetColumn(x);
+
+        var west = h.Where((_, i) => i < x);
+        var east = h.Where((_, i) => i > x);
+
+        var north = v.Where((_, i) => i < y);
+        var south = v.Where((_, i) => i > y);
+
+        return west.All(w => w < t)
+               || south.All(w => w < t)
+               || east.All(w => w < t)
+               || north.All(w => w < t);
+    }
+
+}
